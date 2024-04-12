2c238599ebaca6cb02bd600b9efac23bdcc728b0
Dabana Intenque <Dabanaluintenque55@gmail.com>
2024-04-12 21:52:12 +0000

    coding challenge Array Absolute Value
    Done!

--- src/main/java/ArrayAbsoluteValue.java
+++ src/main/java/ArrayAbsoluteValue.java
@@ -9,6 +9,16 @@ public class ArrayAbsoluteValue {
      * @return the absolute value array of nums.
      */
     public int[] getArrayAbs(int[] nums){
-        return null;
+
+        for (int i = 0; i < nums.length; i ++){
+
+            if(nums[i] < 0) {
+
+                nums[i] = (nums[i] * (-1));
+           
+            }
+            
+        }
+        return nums;
     }
 }
